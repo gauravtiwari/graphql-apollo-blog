274e9cc8a13f65b25cf824cf966ccd0c5e2e0d00
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-14 10:02:21 +0200

    Add faker for seed data and migrate db

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ group :development, :test do
   gem 'byebug'
 end
 
+gem 'faker'
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 3.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+  10.times do |i|
+    User.create!(
+      name: Faker::Name.name,
+      password: 'password',
+      email: "test#{i}@example.com"
+    )
+  end
+
+  100.times do
+    Post.create!(
+      title: Faker::Lorem.sentence,
+      body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, incidunt! Voluptatibus quasi asperiores veritatis nesciunt vitae aliquid, praesentium ratione. Repudiandae, dolor, incidunt. \nAmet corporis porro eveniet rem, eligendi vero, quae.\nScience cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.\nLorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quo autem nisi nobis nemo labore explicabo repellat deserunt harum sequi amet odit, alias reiciendis quia, incidunt vel ullam totam rem?
+      ",
+      user_id: (1..10).to_a.sample
+    )
+  end
+end
