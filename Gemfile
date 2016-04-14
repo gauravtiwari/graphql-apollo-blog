351ee6666395025da2930c93f7eff1fbe92a4628
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-14 13:36:56 +0200

    Fix rendering mismatch

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
+
+gem 'jquery-rails'
+
 # See https://github.com/rails/execjs#readme for more supported runtimes
 gem 'therubyracer', platforms: :ruby
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,9 @@
+//= require jquery
+//= require jquery_ujs
 //= require action_cable
 //= require_tree ./channels
+//= require react
+//= require react-server
 //= require components
 //= require turbolinks
 //= require react_ujs
-
-var React = window.React = global.React = require('react');
-var ReactDOM= window.ReactDOM = global.ReactDOM = require('react-dom');
-window.$ = window.jQuery = require('jquery')
-require('jquery-ujs')
