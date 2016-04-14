d7f31286bb44f9cbeb60422a2aa379a0df8b149e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-14 09:56:43 +0200

    Autload graphql folder

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,21 @@ Bundler.require(*Rails.groups)
 
 module ApolloOnRails
   class Application < Rails::Application
+    config.autoload_paths << Rails.root.join('app/graph')
+    config.autoload_paths << Rails.root.join('app/graph/types')
+    config.active_record.raise_in_transactional_callbacks = true
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.schema_format = :sql
+
+    # Configure rails g to skip helper/assets files
+    config.generators do |g|
+      g.assets = false
+      g.helper = false
+      g.view_specs      false
+      g.helper_specs    false
+    end
+
     config.browserify_rails.commandline_options = "-t babelify --extension=\"es6.js\""
   end
 end
