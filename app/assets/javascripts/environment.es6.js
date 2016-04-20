e6a4b12acf11e2688b8eaa276d923a923f4b10b0
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-20 12:35:01 +0300

    Move helpers to separate folders

--- app/assets/javascripts/environment.es6.js
+++
@@ -1,8 +0,0 @@
-const Environment = () => {
-  return {
-    development: $('meta[name="env"]').attr('env') === "development",
-    production: $('meta[name="env"]').attr('env') === "production"
-  };
-};
-
-module.exports = Environment;
