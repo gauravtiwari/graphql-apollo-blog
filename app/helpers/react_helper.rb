8df3c9d555a9082073cef92ab3642371869fb0c5
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-07-14 17:54:10 +0100

    Update helpers - setTimeout and window

--- app/helpers/react_helper.rb
+++ app/helpers/react_helper.rb
@@ -1,4 +1,10 @@
 module ReactHelper
+  GLOBAL_WRAPPER = <<-JS
+        var global = global || this;
+        var self = self || this;
+        var window = window || this;
+      JS
+
   def react_component(name, props = {}, options = {})
     if options[:prerender]
       block = Proc.new{ concat server_render(name, props) }
@@ -32,8 +38,58 @@ module ReactHelper
     props.is_a?(String) ? props : props.to_json
   end
 
+  def execjs_timer_polyfills
+    <<-JS
+function getStackTrace () {
+var stack;
+try {
+throw new Error('');
+}
+catch (error) {
+stack = error.stack || '';
+}
+stack = stack.split('\\n').map(function (line) { return line.trim(); });
+return stack.splice(stack[0] == 'Error' ? 2 : 1);
+}
+function setInterval() {
+#{undefined_for_exec_js_logging('setInterval')}
+}
+function setTimeout() {
+#{undefined_for_exec_js_logging('setTimeout')}
+}
+function clearTimeout() {
+#{undefined_for_exec_js_logging('clearTimeout')}
+}
+    JS
+  end
+
+  def undefined_for_exec_js_logging(function_name)
+    ""
+  end
+
+  def console_polyfill
+          <<-JS
+var console = { history: [] };
+['error', 'log', 'info', 'warn'].forEach(function (level) {
+  console[level] = function () {
+    var argArray = Array.prototype.slice.call(arguments);
+    if (argArray.length > 0) {
+      argArray[0] = '[SERVER] ' + argArray[0];
+    }
+    console.history.push({level: level, arguments: argArray});
+  };
+});
+          JS
+  end
+
   def server_render(component_name, props)
-    @context = ExecJS.compile(Rails.application.assets["server-bundle.js"].to_s)
+    bundle_js_code = Rails.application.assets["server-bundle.js"].to_s
+    base_js_code = <<-JS
+#{console_polyfill}
+#{execjs_timer_polyfills}
+#{bundle_js_code};
+        JS
+    @context = ExecJS.compile(base_js_code + GLOBAL_WRAPPER +  base_js_code)
     js_code = <<-JS
     (function() {
       var props = #{props_string(props)};
@@ -43,6 +99,6 @@ module ReactHelper
       });
     })()
     JS
-    @context.eval(js_code).html_safe
+    puts @context.eval(js_code).inspect
   end
 end
